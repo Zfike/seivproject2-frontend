328cbd0d911c31f2d2e00c5fd5d709ef6fe12c6b
soup <z.fike@eagles.oc.edu>
2023-09-04 14:41:37 -0500

    Changed the api being used

--- src/services/CourseServices.js
+++ src/services/CourseServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost/api",
+  baseURL: "http://localhost/course-t1",
   withCredentials: false,
   headers: {
     Accept: "application/json",
