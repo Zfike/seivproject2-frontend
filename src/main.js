7e3b613a9a2de9b0249784c288219645610a1300
JaxenM <jxnmcray@gmail.com>
2023-09-04 12:33:20 -0500

    Changed placement of router.js file and references

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router.js";
+import router from "./router/router.js";
 import "./assets/style.css";
 
 createApp(App).use(router).mount("#app");
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 
-import CourseList from "./views/CourseList.vue";
-import AddCourse from "./views/AddCourse.vue";
+import CourseList from "../views/CourseList.vue";
+import AddCourse from "../views/AddCourse.vue";
 // import DeleteCourse from "./views/DeleteCourse.vue";
-import EditCourse from "./views/EditCourse.vue";
+import EditCourse from "../views/EditCourse.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
