4dbafde3dac5f79bc8f2483a43e6075c261f5796
sollydg <141650916+sollydg@users.noreply.github.com>
2023-09-13 20:43:28 -0500

    Revert "cancel test"

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ const title = "Course Listing";
       <ul class="nav-links">
         <div class="menu">
           <li>
-            <button name="cancel" v-on:click.prevent="goToPage()">Go to Page 1</button>
+            <router-link :to="{ name: 'list' }"><a>LIST</a></router-link>
           </li>
           <li>
             <router-link :to="{ name: 'add' }"
